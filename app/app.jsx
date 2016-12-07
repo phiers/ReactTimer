19b2ac0e894fb6c003f07da73dd4d17747922b8b
Paul Hiers Jr <paulhiers@yahoo.com>
2016-12-06 20:54:50 -0500

    support foundation settings

--- app/app.jsx
+++ app/app.jsx
@@ -7,8 +7,9 @@ import Timer from 'Timer';
 import Countdown from 'Countdown';
 
 // Load foundation
-require('style!css!foundation-sites/dist/foundation.min.css')
+
 $(document).foundation();
+// App css
 require('style!css!sass!applicationStyles');
 /* eslint-enable */
 
--- karma.conf.js
+++ karma.conf.js
@@ -5,19 +5,23 @@ module.exports = function(config) {
     browsers: ['Chrome'],
     singleRun: true,
     frameworks: ['mocha'],
-    files: ['app/tests/**/*.test.jsx'],
+    files: [
+      'node_modules/jquery/dist/jquery.min.js',
+      'node_modules/foundation-sites/dist/foundation.min.js',
+      'app/tests/**/*.test.jsx',
+    ],
     preprocessors: {
-      'app/tests/**/*.test.jsx': ['webpack', 'sourcemap']
+      'app/tests/**/*.test.jsx': ['webpack', 'sourcemap'],
     },
     reporters: ['mocha'],
     client: {
       mocha: {
-        timeout: '5000'
-      }
+        timeout: '5000',
+      },
     },
     webpack: webpackConfig,
     webpackServer: {
-      noInfo: true
-    }
+      noInfo: true,
+    },
   });
-}
+};
